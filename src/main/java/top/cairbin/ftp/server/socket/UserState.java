3041d4595b77e8cd7235ced2fc54e32ae02e8d2b
CairBin <cairbin@CairBindeMacBook-Pro.local>
2024-10-18 09:56:01 +0800

    feat: 用户状态

---
+++ src/main/java/top/cairbin/ftp/server/socket/UserState.java
@@ -0,0 +1,17 @@
+/*
+ * @Description: 用户状态枚举
+ * @License: MIT License
+ * @Author: Xinyi Liu(CairBin)
+ * @version: 
+ * @Date: 2024-10-17 07:25:52
+ * @LastEditors: Xinyi Liu(CairBin)
+ * @LastEditTime: 2024-10-17 07:26:18
+ * @Copyright: Copyright (c) 2024 Xinyi Liu(CairBin)
+ */
+package top.cairbin.ftp.server.socket;
+
+public enum UserState {
+    ONLINE,
+    OFFLINE,
+    LOGGING
+}
