2d730a3598104d825dae2a64edc17f002f414a50
CairBin <cairbin@CairBindeMacBook-Pro.local>
2024-10-18 09:51:17 +0800

    feat: 添加初始化程序

--- src/main/java/top/cairbin/ftp/server/App.java
+++ src/main/java/top/cairbin/ftp/server/App.java
@@ -1,25 +1,33 @@
 /*
- * @Description: file content
+ * @Description: 程序入口
  * @License: MIT License
  * @Author: Xinyi Liu(CairBin)
  * @version: 
  * @Date: 2024-10-17 00:10:09
  * @LastEditors: Xinyi Liu(CairBin)
- * @LastEditTime: 2024-10-17 00:42:24
+ * @LastEditTime: 2024-10-18 09:50:48
  * @Copyright: Copyright (c) 2024 Xinyi Liu(CairBin)
  */
 package top.cairbin.ftp.server;
 
-import com.google.inject.Inject;
+import top.cairbin.ftp.server.socket.Server;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         var injector = InjectorFactory.getInjector();
+        Server server = injector.getInstance(Server.class);
+        try {
+            server.createSocket(21);
+            server.setRoot("temp");
+            server.setPasvIp("127.0.0.1");
+            server.setPasvPort(20);
+            server.listener();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 }
