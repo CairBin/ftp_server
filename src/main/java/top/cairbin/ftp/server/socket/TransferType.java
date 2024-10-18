04436fbe9c2c14c11f60dfafd50a74cf5e16ddf4
CairBin <cairbin@CairBindeMacBook-Pro.local>
2024-10-18 09:55:13 +0800

    feat: 文件传输类型，目前ASCII、BINARY

---
+++ src/main/java/top/cairbin/ftp/server/socket/TransferType.java
@@ -0,0 +1,16 @@
+/*
+ * @Description: 传输类型枚举
+ * @License: MIT License
+ * @Author: Xinyi Liu(CairBin)
+ * @version: 1.0.0
+ * @Date: 2024-10-17 07:28:49
+ * @LastEditors: Xinyi Liu(CairBin)
+ * @LastEditTime: 2024-10-17 07:29:02
+ * @Copyright: Copyright (c) 2024 Xinyi Liu(CairBin)
+ */
+package top.cairbin.ftp.server.socket;
+
+public enum TransferType {
+    ASCII,
+    BINARY
+}
