704d3040099e7ea36195a5679cc789217b9305ca
CairBin <cairbin@CairBindeMacBook-Pro.local>
2024-10-18 09:46:49 +0800

    fixed: 修复了解析端口号特殊字符导致异常的bug

--- src/main/java/top/cairbin/ftp/server/utils/FtpCommandParser.java
+++ src/main/java/top/cairbin/ftp/server/utils/FtpCommandParser.java
@@ -5,7 +5,7 @@
  * @version: 1.0.0
  * @Date: 2024-10-17 01:06:34
  * @LastEditors: Xinyi Liu(CairBin)
- * @LastEditTime: 2024-10-17 01:35:55
+ * @LastEditTime: 2024-10-18 09:45:57
  * @Copyright: Copyright (c) 2024 Xinyi Liu(CairBin)
  */
 package top.cairbin.ftp.server.utils;
@@ -18,14 +18,16 @@ public class FtpCommandParser {
         }
 
         try{
+            // System.out.println("Msg command--->"+msg[0]);
             FtpCommand cmd = FtpCommand.valueOf(msg[0].toUpperCase());
+            // System.out.println("CMD--->" + cmd.name());
             Object args =  null;
             switch(cmd){
                 case USER:
                     args = msg[1];
                     break;
                 case PASS:
-                    args = msg[1];
+                    if(msg.length >= 2) args = msg[1];
                     break;
                 case CWD:
                     args = msg[1];
@@ -37,6 +39,9 @@ public class FtpCommandParser {
                     if(msg.length >= 2) args = msg[1];
                     break;
                 case NLIST:
+                    if(msg.length >= 2) args = msg[1];
+                    break;
+                case NLST:
                     if(msg.length >= 2) args = msg[1];
                     break;
                 case MKD:
@@ -60,11 +65,11 @@ public class FtpCommandParser {
                     // 前四个部分为IP地址
                     String ipAddress = String.join(".", parts[0], parts[1], parts[2], parts[3]);
                     // 最后两个部分为端口号
-                    int p1 = Integer.parseInt(parts[4]);
-                    int p2 = Integer.parseInt(parts[5]);
+                    int p1 = Integer.parseInt(parts[4].trim().replaceAll("\r|\n", ""));
+                    int p2 = Integer.valueOf(parts[5].trim().replaceAll("\r|\n", "")).intValue();
                     // 计算端口号
                     int port = p1 * 256 + p2;
-                    args = String.format("%s:%s", ipAddress, port);
+                    args = String.format("%s:%d", ipAddress, port);
                     break;
                 case RETR:
                     args = msg[1];
@@ -90,6 +95,7 @@ public class FtpCommandParser {
             
             return new FtpParams(cmd, args);
         }catch(Exception e){
+            System.out.println("Error occurred: " + e.getMessage());
             return new FtpParams(FtpCommand.NOOP, null);
         }
 
